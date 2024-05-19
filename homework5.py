8c0b6f2fa937c1ae60d28dc724d22b04fdc4cca6
GenesisXabsolute <and9296244800@yandex.ru>
2024-05-19 22:11:18 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,13 @@
+my_list = ['apple', 'orange', 'banana', 'grape', 'pear', 'grapefruit']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'pear'
+print(my_list)
+my_dict = {'red': 'красный', 'green': 'зелёный', 'yellow': 'жёлтый'}
+print(my_dict)
+print(my_dict['red'])
+my_dict['red'] = 'no red'
+my_dict['white'] = 'белый'
+print(my_dict)

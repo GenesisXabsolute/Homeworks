b7e04b497e585a4a045c9359752ce2e84fde5b8a
GenesisXabsolute <and9296244800@yandex.ru>
2024-05-22 17:47:25 +0300

    Add files via upload

---
+++ Домашняя работа по уроку "Цикл for. Элементы списка. Полезные функции в цикле"/Homework (for).py
@@ -0,0 +1,7 @@
+list_ = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+for i in range(len(list_)):
+    print(f'Я ежу на автомобиле марки {list_[i]}')
+cars_count = 0
+for i in range(len(list_)):
+    cars_count += 10
+    

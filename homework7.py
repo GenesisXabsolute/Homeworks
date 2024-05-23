2e8ca3c04fc6348ba91b452b396df123c53e513f
GenesisXabsolute <and9296244800@yandex.ru>
2024-05-23 19:22:57 +0300

    Add files via upload

---
+++ homework7.py
@@ -0,0 +1,14 @@
+my_dict = {'Вася': 2001, 'Ваня': 2007, 'Петя': 2010}
+print(my_dict)
+print(my_dict['Ваня'])
+print(my_dict.get('Толя'))
+my_dict.update({'Надя': 1999, 'Федя': 2005})
+print(my_dict.pop('Надя'))
+print(my_dict)
+my_set = {12, 'строка', True}
+my_set.update([34, 56])
+my_set.remove(34)
+print(my_set)
+
+
+

fe242432bdc33f1b8b5a3002738b053745ccc700
GenesisXabsolute <and9296244800@yandex.ru>
2024-05-28 21:08:45 +0300

    Add files via upload

---
+++ Специальные методы классов.py
@@ -0,0 +1,9 @@
+class House:
+    def __init__(self):
+        self.number_of_floors = 0
+
+    def set_new_number_of_floors(self, floors):
+        self.number_of_floors = floors
+        print(self.number_of_floors)
+
+

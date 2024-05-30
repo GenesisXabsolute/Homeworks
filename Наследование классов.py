a338b93911c7e543187ba184c9b09882747b08a8
GenesisXabsolute <and9296244800@yandex.ru>
2024-05-30 15:26:42 +0300

    Add files via upload

---
+++ Наследование классов.py
@@ -0,0 +1,19 @@
+class Car:
+    price = 1000000
+
+    def horse_power(self, n):
+        return n
+
+
+class Nissan(Car):
+    price = 2500000
+
+    def horse_power(self, n):
+        return n
+
+
+class Kia(Car):
+    price = 3200000
+
+    def horse_power(self, n):
+        return n

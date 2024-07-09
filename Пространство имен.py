9b2d4be6f8e4c0e3d3dbcc7e6c4e9c42783329a6
GenesisXabsolute <and9296244800@yandex.ru>
2024-07-10 00:28:33 +0300

    Add files via upload

--- Пространство имен.py
+++ Пространство имен.py
@@ -1,8 +1,27 @@
-def test_function():
-    def inner_function():
-        print('Я в области видимости функции test_function')
-    inner_function()
+calls = 0
 
 
-test_function()
-#inner_function() Не получается вызвать
+def count_calls():
+    global calls
+    calls += 1
+
+
+def string_info(string):
+    global calls
+    calls += 1
+    return len(string), string.upper(), string.lower()
+
+
+def is_contains(string, points):
+    global calls
+    calls += 1
+    string1 = string.upper()
+    points1 = [i.upper() for i in points]
+    return string1 in points1
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
+

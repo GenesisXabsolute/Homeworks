2c9057269a58bc17dd0d5dacc7886b6674f3b34d
GenesisXabsolute <and9296244800@yandex.ru>
2024-07-10 19:24:00 +0300

    Add files via upload

---
+++ Позиционирование в файле.py
@@ -0,0 +1,26 @@
+import io
+from pprint import pprint
+
+
+def custom_write(file_name, strings):
+    file = open(file_name, 'w', encoding='utf8')
+    s = 0
+    dict1 = dict()
+    for i in strings:
+        s += 1
+        dict1[(s, file.tell())] = i
+        file.write(i + '\n')
+    file.close()
+    return dict1
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)

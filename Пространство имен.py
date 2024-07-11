506566f5349b0f0265c7f837bfac0dc396cd4cb0
GenesisXabsolute <and9296244800@yandex.ru>
2024-07-11 19:52:15 +0300

    Add files via upload

---
+++ Пространство имен.py
@@ -0,0 +1,25 @@
+calls = 0
+
+
+def count_calls():
+    global calls
+    calls += 1
+
+
+def string_info(string):
+    count_calls()
+    return len(string), string.upper(), string.lower()
+
+
+def is_contains(string, points):
+    count_calls()
+    string1 = string.upper()
+    points1 = [i.upper() for i in points]
+    return string1 in points1
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
+

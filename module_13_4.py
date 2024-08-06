03656c063f43612ba08c11417418faa7ad62d6cf
GenesisXabsolute <and9296244800@yandex.ru>
2024-08-06 20:25:02 +0300

    Add files via upload

---
+++ module_13_4.py
@@ -0,0 +1,52 @@
+from aiogram import Bot, Dispatcher, types, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+import asyncio
+
+api = ''
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+class UserStates(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+@dp.message_handler(commands=['start'])
+async def start(msg):
+    await msg.answer('Привет! Я бот помогающий твоему здоровью.')
+
+
+@dp.message_handler(text=['Calories'])
+async def set_age(message):
+    await message.answer('Введите свой возраст')
+    await UserStates.age.set()
+
+
+@dp.message_handler(state=[UserStates.age])
+async def set_growth(message, state):
+    await state.update_data(age=message.text)
+    data = await state.get_data()
+    await message.answer('Введите свой рост')
+    await UserStates.growth.set()
+
+
+@dp.message_handler(state=[UserStates.growth])
+async def set_weight(message, state):
+    await state.update_data(growth=message.text)
+    await message.answer('Введите свой вес')
+    await UserStates.weight.set()
+
+
+@dp.message_handler(state=[UserStates.weight])
+async def send_calories(message, state):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    await message.answer(f"Ваша норма калорий {(10 * int(data['weight'])) + (6.25 * int(data['growth'])) - (5 * int(data['age'])) + 5}")
+    await state.finish()
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
